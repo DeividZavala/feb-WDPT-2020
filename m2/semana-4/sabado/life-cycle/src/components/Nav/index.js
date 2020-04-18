3cb5cc4b452194a50041b31480ccb477d46dc849
DeividZavala <crusedmoss@gmail.com>
2020-04-18 11:26:12 -0500

    navbar component changes

--- m2/semana-4/sabado/life-cycle/src/components/Nav/index.js
+++ m2/semana-4/sabado/life-cycle/src/components/Nav/index.js
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
+import "./navbar.css";
 
 class Nav extends Component {
   render() {
     return (
-      <nav class="uk-navbar-container" uk-navbar>
-        <div class="uk-navbar-left">
-          <ul class="uk-navbar-nav">
-            <li class="uk-active">
+      <nav className="uk-navbar-container navbar" uk-navbar="true">
+        <div className="uk-navbar-left">
+          <ul className="uk-navbar-nav">
+            <li className="uk-active">
               <a href="#">Active</a>
             </li>
             <li>
               <a href="#">Parent</a>
-              <div class="uk-navbar-dropdown">
-                <ul class="uk-nav uk-navbar-dropdown-nav">
-                  <li class="uk-active">
+              <div className="uk-navbar-dropdown">
+                <ul className="uk-nav uk-navbar-dropdown-nav">
+                  <li className="uk-active">
                     <a href="#">Active</a>
                   </li>
                   <li>
--- m2/semana-4/sabado/life-cycle/src/index.js
+++ m2/semana-4/sabado/life-cycle/src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import UIkit from "uikit";
+import "uikit/dist/css/uikit.min.css";
+import Icons from "uikit/dist/js/uikit-icons";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+UIkit.use(Icons);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

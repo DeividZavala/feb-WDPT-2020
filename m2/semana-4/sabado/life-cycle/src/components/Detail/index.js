a67ac490fc531fcb9eed24c5b41cbd631ec069a3
DeividZavala <crusedmoss@gmail.com>
2020-04-18 15:57:12 -0500

    componentWillReceaveProps

--- m2/semana-4/sabado/life-cycle/src/components/Detail/index.js
+++ m2/semana-4/sabado/life-cycle/src/components/Detail/index.js
@@ -1,9 +1,32 @@
 import React, { Component } from "react";
+import { getCharacter } from "../../services/characterService";
 
 class Detail extends Component {
+  state = {
+    character: {},
+  };
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    getCharacter(id).then((res) => {
+      console.log(res.data);
+      const { data: character } = res;
+      this.setState({ character });
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.match.params.id !== this.props.match.params.id) {
+      const { id } = nextProps.match.params;
+      getCharacter(id).then((res) => {
+        console.log(res.data);
+        const { data: character } = res;
+        this.setState({ character });
+      });
+    }
+  }
+
   render() {
-    console.log(this.props);
-    return <h1>Id: {this.props.match.params.id}</h1>;
+    return <h1>{this.state.character.name}</h1>;
   }
 }
 
--- m2/semana-4/sabado/life-cycle/src/services/characterService.js
+++ m2/semana-4/sabado/life-cycle/src/services/characterService.js
@@ -3,3 +3,7 @@ import axios from "axios";
 export const getCharacters = () => {
   return axios.get("https://rickandmortyapi.com/api/character");
 };
+
+export const getCharacter = (id) => {
+  return axios.get(`https://rickandmortyapi.com/api/character/${id}`);
+};

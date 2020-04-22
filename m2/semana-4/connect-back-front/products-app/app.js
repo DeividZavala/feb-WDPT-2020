b1e2e22152d8525d3d1a7b1f64dfabc843794f48
DeividZavala <crusedmoss@gmail.com>
2020-04-21 19:56:22 -0500

    basic config

---
+++ m2/semana-4/connect-back-front/products-app/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/products-app", {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+const indexRouter = require("./routes/index");
+const productsRouter = require("./routes/products");
+app.use("/", indexRouter);
+app.use("/products", productsRouter);
+
+module.exports = app;

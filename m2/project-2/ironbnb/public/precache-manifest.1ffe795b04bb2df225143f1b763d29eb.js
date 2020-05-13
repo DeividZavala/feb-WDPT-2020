bb7845f35b9c32d96c1bf74f87702946c7933d44
DeividZavala <crusedmoss@gmail.com>
2020-05-12 19:44:32 -0500

    commands for prepare the application for deploy

---
+++ m2/project-2/ironbnb/public/precache-manifest.1ffe795b04bb2df225143f1b763d29eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4afc16501046e658f5b5f7702e340cff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31e3ea09fe635ee0e17e",
+    "url": "/static/css/2.8eff4dd6.chunk.css"
+  },
+  {
+    "revision": "23f05698154c2563b35e",
+    "url": "/static/css/main.1c17c2fe.chunk.css"
+  },
+  {
+    "revision": "31e3ea09fe635ee0e17e",
+    "url": "/static/js/2.2344188c.chunk.js"
+  },
+  {
+    "revision": "b30d6cda143a61be38d419b368c7eb9d",
+    "url": "/static/js/2.2344188c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23f05698154c2563b35e",
+    "url": "/static/js/main.fc9392e8.chunk.js"
+  },
+  {
+    "revision": "4bd98b88234245f3e8a9",
+    "url": "/static/js/runtime-main.18a38da9.js"
+  }
+]);

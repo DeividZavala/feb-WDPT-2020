7ab829ccc3f73aff925de19581500ba0fcdcd5d7
David <crusedmoss@gmail.com>
2020-06-25 01:44:03 -0500

    removing components and imports

--- m3/project-3/ironcommerce-front/src/App.js
+++ m3/project-3/ironcommerce-front/src/App.js
@@ -1,26 +1,11 @@
 import React from "react";
 import logo from "./logo.svg";
-import Navbar from "~commons/NavBar";
 import "./App.css";
-import Product from "~commons/Product";
-import Cart from "~components/Cart";
-import CheckoutTable from "~components/CheckoutTable";
-import Section from "~commons/Section";
 
 function App() {
   return (
     <div className="App">
-      <Cart />
-      <Navbar />
-      <Section>
-        <div className="uk-grid uk-child-width-1-4">
-          <Product />
-        </div>
-      </Section>
-      <Section>
-        <CheckoutTable />
-      </Section>
-      {/* <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -33,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header> */}
+      </header>
     </div>
   );
 }
